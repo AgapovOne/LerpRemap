6b46ccc2b4ad8a76941f8f683e4650ad31d17220
AgapovOne <agapov.one@gmail.com>
2023-03-20 14:54:08 +0500

    Initial Commit

---
+++ Interpolation/InterpolationApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterpolationApp.swift
+//  Interpolation
+//
+//  Created by Alex Agapov on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct InterpolationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
